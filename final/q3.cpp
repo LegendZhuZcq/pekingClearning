c9c55fb21ea1a7f15277aae75733e47ec31c4442
LegendZhuZcq <zhu.legend@gmail.com>
2017-07-25 00:05:39 -0700

    finished course1

---
+++ final/q3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int n;
+	double x[100], y[100], dis=0;
+	cin >> n;
+	for (int i = 0; i < n;i++) {
+		cin >> x[i] >> y[i];
+		for (int j = 0; j < i + 1; j++) {
+			double currentDis = sqrt(pow((x[i] - x[j]),2)+ pow((y[i] - y[j]),2));
+			(currentDis > dis) ? dis = currentDis:dis=dis;
+		}
+
+	}
+	cout << fixed << setprecision(4) << dis << endl;
+	return 0;
+}
---
+++ final/q4.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int calculator(int num1, int num2, char operation) {
+	switch (operation) {
+	case '+':
+		cout << (num1 + num2);
+		break;
+	case '-':
+		cout << (num1 + num2);
+		break;
+	case '*':
+		cout << (num1 * num2);
+		break;
+	case '/':
+		if (num2 == 0)
+		{
+			cout << ("Divided by zero!");
+		}
+		else {
+			cout << (num1 / num2);
+		}
+		break;
+	default:
+		cout << "Invalid operator!";
+	}
+	return 0;
+}
+
+
+int main() {
+	int a, b;
+	char c;
+	cin >> a >> b >> c;
+	calculator(a, b, c);
+
+	return 0;
+}
